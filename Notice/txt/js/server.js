92379f6a8046e40d1054af5356d9295470cd4ecc
mohuajz <118093026+mohuajz@users.noreply.github.com>
2022-12-19 11:41:35 +0800

    Add files via upload

---
+++ Notice/txt/js/server.js
@@ -0,0 +1,18 @@
+jsonp_callback(
+    {
+        "latestRelease": {
+            "versionCode": 7,
+            "versionName": "1.0.0",
+            "date": "2022.4.25",
+            "url": ["https://wwc.lanzoub.com/iUVrn03re25c", "https://github.com/Tungstend/HMCL-PE/blob/main/HMCLPE/release/HMCLPE-release.apk"],
+            "updateLog": "<font color=\"#FF0000\">好</font><font color=\"#DF2000\">耶</font><font color=\"#BF4000\">，</font><font color=\"#9F6000\">是</font><font color=\"#7F8000\">新</font><font color=\"#5FA000\">版</font><font color=\"#3FC000\">本</font><font color=\"#1FE000\">。</font>"
+        },
+        "latestPrerelease": {
+            "versionCode": 5,
+            "versionName": "1.0.0",
+            "date": "2022.4.11",
+            "url": ["https://wwc.lanzoub.com/iWcLW030az9i", "https://github.com/Tungstend/HMCL-PE/blob/main/HMCLPE/release/HMCLPE-release.apk"],
+            "updateLog": "好耶，是新版本。(pre)"
+        }
+    }
+);
